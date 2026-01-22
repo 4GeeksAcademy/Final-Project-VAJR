73e7ddb4fc5d5141fbd646649f94459a67926428
Andrea Mujica <andreamm0714@gmail.com>
2026-01-22 00:15:52 +0000

    [Updated endpoint /pacient]
    -app.py: Added get, put, login and signup functions

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db,Pacient,Doctors,Appointments,Availability
+from api.models import db, Pacient, Doctors, Appointments, Availability
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
@@ -70,6 +70,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -78,13 +80,15 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
+
 @app.route('/pacient/signup', methods=['POST'])
 def pacient_signup():
     request_body = request.get_json(silent=True)
     if request_body is None:
         return jsonify({'msg': 'Body is empty'}), 400
 
-    existing_email = Pacient.query.filter_by(email=request_body['email']).first()
+    existing_email = Pacient.query.filter_by(
+        email=request_body['email']).first()
     if existing_email:
         return jsonify({'msg': 'Pacient already exists'}), 400
     if 'name' not in request_body:
@@ -92,20 +96,23 @@ def pacient_signup():
     if 'email' not in request_body:
         return jsonify({'msg': 'Please provide an email'}), 400
 
-    existing_email = Pacient.query.filter_by(email=request_body['email']).first()
+    existing_email = Pacient.query.filter_by(
+        email=request_body['email']).first()
     if existing_email:
         return jsonify({'msg': 'Email already registered'}), 400
 
     if 'password' not in request_body:
         return jsonify({'msg': 'Please provide a password'}), 400
 
-    pw_hash = bcrypt.generate_password_hash(request_body['password']).decode('utf-8')
+    pw_hash = bcrypt.generate_password_hash(
+        request_body['password']).decode('utf-8')
     new_pacient = Pacient(email=request_body['email'],
-                    password=pw_hash, name=request_body['name'], is_active=True)
+                          password=pw_hash, name=request_body['name'], is_active=True)
 
     pacients_phone = None
     if 'phone' in request_body and request_body['phone']:
-        existing_phone = Pacient.query.filter_by(phone=request_body['phone']).first()
+        existing_phone = Pacient.query.filter_by(
+            phone=request_body['phone']).first()
         if existing_phone:
             return jsonify({'msg': 'Phone already in use'}), 400
         pacients_phone = request_body['phone']
@@ -115,21 +122,23 @@ def pacient_signup():
     db.session.commit()
     return jsonify({'msg': 'New user created successfully'}), 201
 
+
 @app.route('/pacient/login', methods=['POST'])
 def pacient_login():
     request_body = request.get_json(silent=True)
     if request_body is None:
-        return jsonify ({'msg': 'The body is empty'}), 400
+        return jsonify({'msg': 'The body is empty'}), 400
     if 'password' not in request_body and 'email' not in request_body:
-        return jsonify ({'msg': 'You must provide an email and password'}), 400
-    
+        return jsonify({'msg': 'You must provide an email and password'}), 400
+
     pacient = Pacient.query.filter_by(email=request_body['email']).first()
     if pacient is None:
-        return jsonify ({'msg': 'Invalid email or password'}), 400
+        return jsonify({'msg': 'Invalid email or password'}), 400
 
-    pw_check = bcrypt.check_password_hash(pacient.password, request_body['password'])
+    pw_check = bcrypt.check_password_hash(
+        pacient.password, request_body['password'])
     if pw_check == False:
-        return jsonify ({'msg': 'Invalid email or password'}), 400
+        return jsonify({'msg': 'Invalid email or password'}), 400
 
     token = create_access_token(identity=pacient.id)
     return jsonify({
@@ -137,40 +146,44 @@ def pacient_login():
         'token': token
     }), 200
 
+
 @app.route('/pacient', methods=['GET'])
 @jwt_required()
 def get_pacient():
-    pacient_id = get_jwt_identity() 
+    pacient_id = get_jwt_identity()
     pacient = Pacient.query.get(pacient_id)
-    
+
     if pacient is None:
         return jsonify({'msg': 'Patient not found'}), 404
 
     return jsonify(pacient.serialize()), 200
-    
+
+
 @app.route('/pacient', methods=['PUT'])
 @jwt_required()
 def update_pacient_info():
     request_body = request.get_json(silent=True)
     if request_body is None:
         return jsonify({'msg': 'Body is empty'}), 400
-    
-    pacient_id = get_jwt_identity() 
+
+    pacient_id = get_jwt_identity()
     pacient = Pacient.query.get(pacient_id)
     if pacient is None:
         return jsonify({'msg': 'Pacient does not exist'}), 404
-    
+
     if 'name' in request_body:
         pacient.name = request_body['name']
 
     if 'email' in request_body and request_body['email'] != pacient.email:
-        existing_email = Pacient.query.filter_by(email=request_body['email']).first()
+        existing_email = Pacient.query.filter_by(
+            email=request_body['email']).first()
         if existing_email:
             return jsonify({'msg': 'This email is already in use by another account'}), 400
         pacient.email = request_body['email']
 
     if 'phone' in request_body and request_body['phone'] != pacient.phone:
-        existing_phone = Pacient.query.filter_by(phone=request_body['phone']).first()
+        existing_phone = Pacient.query.filter_by(
+            phone=request_body['phone']).first()
         if existing_phone:
             return jsonify({'msg': 'This phone is already in use by another account'}), 400
         pacient.phone = request_body['phone']
@@ -178,10 +191,11 @@ def update_pacient_info():
     if 'new_password' in request_body and 'old_password' in request_body:
         if not bcrypt.check_password_hash(pacient.password, request_body['old_password']):
             return jsonify({'msg': 'Old password is incorrect'}), 401
+        pacient.password = bcrypt.generate_password_hash(
+            request_body['new_password']).decode('utf-8')
     elif 'new_password' in request_body and 'old_password' not in request_body:
         return jsonify({'msg': 'To change password, you must provide your old password'}), 400
-    
-    pacient.password = bcrypt.generate_password_hash(request_body['new_password']).decode('utf-8')
+
     db.session.commit()
     return jsonify({'msg': 'Profile updated successfully', 'data': pacient.serialize()}), 200
 
