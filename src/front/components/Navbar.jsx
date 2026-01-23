65c943ef362ab5a70fbd7cc09b4e39f4151f838d
Andrea Mujica <andreamm0714@gmail.com>
2026-01-23 19:22:23 +0000

    [Updated Navbar]
    -Navbar.jsx: Added a logo, name of the webpage and view without token (Log in, and Sign up buttons)

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,16 +3,44 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar bg-none" data-bs-theme="light">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<Link to="/" className="text-decoration-none">
+					<i class="fa-solid fa-house-medical" style={{color: "#1a5799", fontSize: "30px"}}></i>
+					<span className="navbar-brand mb-0 h1 fs-3 ms-2">HiDoc</span>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				<button type="button" className="btn nav-item dropdown ms-auto me-2">
+					<a className="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Log in
+					</a>
+					<ul className="dropdown-menu dropdown-menu-end mt-2 shadow">
+						<p className="ms-4 mb-0"><b>Doctors</b></p>
+						<li>
+							<Link className="dropdown-item ms-2">Log in</Link>
+						</li>
+						<li><hr className="dropdown-divider"/></li>
+						<p className="ms-4 mb-0"><b>Pacients</b></p>
+						<li>
+							<Link className="dropdown-item ms-2">Log in</Link>
+						</li>
+					</ul>
+				</button> 
+				<button type="button" className="btn nav-item dropdown" style={{backgroundColor: "#1A5799", color: "#E9F5FF"}}>
+					<a className="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Sign up
+					</a>
+					<ul className="dropdown-menu dropdown-menu-end mt-2 shadow">
+						<p className="ms-4 mb-0"><b>Doctors</b></p>
+						<li>
+							<Link className="dropdown-item ms-2">Sign up</Link>
+						</li>
+						<li><hr className="dropdown-divider"/></li>
+						<p className="ms-4 mb-0"><b>Pacients</b></p>
+						<li>
+							<Link className="dropdown-item ms-2">Sign up</Link>
+						</li>
+					</ul>
+				</button>
 			</div>
 		</nav>
 	);
