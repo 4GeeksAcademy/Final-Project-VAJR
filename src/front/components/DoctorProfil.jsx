dc28d2363e207cee08c63dc278b1a29c49ebfbda
cinjude <110071132+cinjude@users.noreply.github.com>
2026-01-25 16:09:04 +0000

    [add style in doctor card]

--- src/front/components/DoctorProfil.jsx
+++ src/front/components/DoctorProfil.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react"
+import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLocationDot, faCalendar } from "@fortawesome/free-solid-svg-icons"
 
 export const DoctorProfile = () => {
 
@@ -31,46 +34,49 @@ export const DoctorProfile = () => {
     console.log(store.doctors)
 
     return (
-        <div className="doctor-card row">
 
-            <ul className="doctor-card-ul d-flex justify-content-between">
-                {
-                    store.doctors.map((item) =>
-                    (
-                        <li className="p-3 doctor" style={{ width: "338px", borderRadius: "10px" }} key={item.id}>
-                            <div className="d-flex ">
-                                <img
-                                    src={item.picture}
-                                    alt="imagen"
-                                    style={{
-                                        width: "90px",
-                                        height: "90px",
-                                        borderRadius: "50%",
-                                        objectFit: "cover"
-                                    }}
-                                />
-                                <div className="ms-3">
-                                    <h4> Dr. {item.name} </h4>
-                                    <p className="text-center" style={{ color: "#468BE6" }}>{item.specialties}</p>
+        <div className="doctor-card mt-4">
+            <Link to={"/doctorpage/"} className="text-decoration-none"  style={{ color: "#121212" }}>
+                <ul className="doctor-card-ul d-flex justify-content-between overflow-auto overflow-x-auto flex-sm-nowrap">
+                    {
+                        store.doctors.map((item) =>
+                        (
+                            <li className="p-3 doctor flex-shrink-0" style={{ width: "338px", borderRadius: "10px" }} key={item.id}>
+                                <div className="d-flex ">
+                                    <img
+                                        src={item.picture}
+                                        alt="imagen"
+                                        style={{
+                                            width: "90px",
+                                            height: "90px",
+                                            borderRadius: "50%",
+                                            objectFit: "cover"
+                                        }}
+                                    />
+                                    <div className="ms-3 mbs">
+                                        <h4> Dr. {item.name} </h4>
+                                        <p className="text-center mt-1" style={{ color: "#468BE6" }}>{item.specialties}</p>
+                                    </div>
+
                                 </div>
-
-                            </div>
-                            <div>
-                                <div className="text-center">
-                                    <p>Location??</p>
-                                    <p style={{ color: "#03eb48" }} >New patient appts</p>
-                                    <p>Next available today:</p>
+                                <div className="pb-2">
+                                    <div className="text-center mbs">
+                                        <p className="mb-2 "><span style={{ color: "#468BE6" }}><FontAwesomeIcon icon={faLocationDot} /></span> Location??</p>
+                                        <p className="mb-2 fw-semibold " style={{ color: "#27fb8a" }} > <span><FontAwesomeIcon icon={faCalendar} /> </span> New patient appts</p>
+                                        <p className="mb-2 ">Next available today:</p>
+                                    </div>
+                                    <p>Check tomorrow</p>
                                 </div>
-                                <p>Check tomorrow</p>
-                            </div>
-                            <h5 className="button-card-doctor text-center" style={{ backgroundColor: "#1A5799", color: "#E9F5FF" }}>Book online</h5>
+                                <h5 className="button-card-doctor text-center" style={{ backgroundColor: "#1A5799", color: "#E9F5FF" }}>Book online</h5>
 
 
-                        </li>
-                    )
-                    )
-                }
-            </ul>
+                            </li>
+                        )
+                        )
+                    }
+                </ul>
+            </Link>
+
 
         </div>
     )
