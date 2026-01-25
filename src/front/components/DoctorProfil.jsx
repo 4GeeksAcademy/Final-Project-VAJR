6033609771a8221543997b513de1a9636a797cec
cinjude <110071132+cinjude@users.noreply.github.com>
2026-01-25 06:17:33 +0000

    [add los doctores tarjetas]

--- src/front/components/DoctorProfil.jsx
+++ src/front/components/DoctorProfil.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import useGlobalReducer from "../hooks/useGlobalReducer"
+
 export const DoctorProfile = () => {
 
     const { store, dispatch } = useGlobalReducer()
@@ -28,8 +29,49 @@ export const DoctorProfile = () => {
     }, [])
 
     console.log(store.doctors)
-    return (
 
-        <div> from home DoctorProfile</div>
+    return (
+        <div className="doctor-card row">
+
+            <ul className="doctor-card-ul d-flex justify-content-between">
+                {
+                    store.doctors.map((item) =>
+                    (
+                        <li className="p-3 doctor" style={{ width: "338px", borderRadius: "10px" }} key={item.id}>
+                            <div className="d-flex ">
+                                <img
+                                    src={item.picture}
+                                    alt="imagen"
+                                    style={{
+                                        width: "90px",
+                                        height: "90px",
+                                        borderRadius: "50%",
+                                        objectFit: "cover"
+                                    }}
+                                />
+                                <div className="ms-3">
+                                    <h4> Dr. {item.name} </h4>
+                                    <p className="text-center" style={{ color: "#468BE6" }}>{item.specialties}</p>
+                                </div>
+
+                            </div>
+                            <div>
+                                <div className="text-center">
+                                    <p>Location??</p>
+                                    <p style={{ color: "#03eb48" }} >New patient appts</p>
+                                    <p>Next available today:</p>
+                                </div>
+                                <p>Check tomorrow</p>
+                            </div>
+                            <h5 className="button-card-doctor text-center" style={{ backgroundColor: "#1A5799", color: "#E9F5FF" }}>Book online</h5>
+
+
+                        </li>
+                    )
+                    )
+                }
+            </ul>
+
+        </div>
     )
 }
