1b24b5f9e5c5de44f851aa448313d6425722656e
cinjude <110071132+cinjude@users.noreply.github.com>
2026-02-02 23:55:08 +0000

    Revert "feat: add Leaflet map and calendar dependencies"
    This reverts commit a8ac420e36c44add2cd253c658c1bcf5da42dd9c, reversing
    changes made to 50d875ae793077d67dfe72d270ed31904fe9378c.

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -7,6 +7,7 @@ import { StoreProvider } from './hooks/useGlobalReducer';  // Import the StorePr
 import { BackendURL } from './components/BackendURL';
 import "leaflet/dist/leaflet.css"
 
+
 const Main = () => {
     
     if(! import.meta.env.VITE_BACKEND_URL ||  import.meta.env.VITE_BACKEND_URL == "") return (
