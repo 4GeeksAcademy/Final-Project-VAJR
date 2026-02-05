c842b95fc99806eab3f20c1a3ca53018824b761a
cinjude <110071132+cinjude@users.noreply.github.com>
2026-02-05 21:35:01 +0000

    [doctor profile]

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -14,6 +14,7 @@ import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { LoginDoctor } from "./pages/LoginDoctor";  
 import { SignupDoctor } from "./pages/SigunpDoctor";
+import { DoctorPage } from "./pages/DoctorPage";
 
 
 export const router = createBrowserRouter(
@@ -36,7 +37,7 @@ export const router = createBrowserRouter(
         <Route path="/pacient/login" element={<Login />} />
         <Route path="/doctor/register" element={<SignupDoctor />} />
         <Route path="/doctor/login" element={<LoginDoctor />} />
-        
+         <Route path="/dotorpage/:doctorId" element={ <DoctorPage />} />
       </Route>
     )
 );
